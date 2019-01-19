4b7a63ec69af7bacc2375e33dd11126dd2f8a5dc
lw <me@lw.ag>
2019-01-19 17:07:31 +0100

    Update basic beatmap test to be functional

--- test/main.c
+++ test/main.c
@@ -1,15 +1,19 @@
+#include "athenosum/game.h"
 #include "athenosum/beatmap.h"
 
 #include <stdio.h>
 
 int main()
 {
-	char *map = NULL;
-	size_t read = find_beatmap(OSU_DEFAULT_PATH, "raja", &map);
+	char *default_path;
+	if (!(get_osu_path(&default_path))) {
+		printf("failed fetching osu! path\n");
+	} else printf("got osu! path: '%s'\n", default_path);
 
-	printf("%s\n", map);
-
-	free(map);
+	char *map;
+	if (!(find_beatmap(default_path, "raja - the li", &map))) {
+		printf("couldn't find beatmap\n");
+	} else printf("found beatmap at '%s'\n", map);
 
 	return 0;
 }
